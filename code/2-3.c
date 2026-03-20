3c280087975c2935c7820543c0be466ed791912e
Seungminnnnn <114681724+Seungminnnnn@users.noreply.github.com>
2026-03-20 11:16:54 +0900

    Add division function and main program

---
+++ code/2-3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void divide(int a, int b, int *q, int *r) {
+	if (b == 0) {
+		*q = 0;
+        *r = 0;
+	} else {
+		*q = a /b;
+		*r = a % b;
+	}
+}
+
+int main() {
+	int a = 27;
+	int b = 4;
+	int q = 0;
+	int r = 0;
+
+	divide (a, b, &q, &r);
+	
+	printf("%d\n%d\n", q, r);
+
+	return 0;
+}
